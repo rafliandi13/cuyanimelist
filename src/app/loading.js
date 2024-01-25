91f7a357efed77ae2f79408cbb3fc9a55498374d
rafli_andi13 <herp1306@gmail.com>
2024-01-25 21:56:40 +0700

    sesi 3 selesai(UI Theming & Loading bar)

---
+++ src/app/loading.js
@@ -0,0 +1,12 @@
+const Loading = () => {
+    return (
+        // <div>
+        //     <div>LOADING .. </div>
+        // </div>
+        <div className="flex justify-center items-center min-h-screen">
+            <div className="flex justify-center items-center loading"></div>
+        </div>
+    )
+}
+
+export default Loading
