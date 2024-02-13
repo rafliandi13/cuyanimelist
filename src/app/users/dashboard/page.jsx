ecab17d586f9aa69f3d8e6b8d5a2a20c5195b2b1
rafli_andi13 <herp1306@gmail.com>
2024-02-13 23:37:37 +0700

    Sesi 9 Perbaikan UI dashboard

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,15 +1,21 @@
 import { authUserSession } from "@/libs/auth-libs"
 import Image from "next/image"
+import Link from "next/link"
 
 const Page = async() => {
     const user = await authUserSession()
     console.log(user)
 
     return(
-        <div className="text-color-primary">
-            <h3>Dashboard</h3>
-            <h5>Welcolme, {user?.name}</h5>
+        <div className="mt-8 text-color-primary flex flex-col justify-center items-center">
+            <h5 className="text-2xl font-bold">Welcolme, {user?.name}</h5>
             <Image src={user?.image} width={250} height={250}/>
+            <div className="flex flext-wrap gap-4 py-9">
+                <Link href="/users/dashboard/collection"
+                className="bg-color-accent text-color-dark font-bold px-3 py-3 text-xl">My Collection</Link>
+                <Link href="/users/dashboard/comment"
+                className="bg-color-accent text-color-dark font-bold px-3 py-3 text-xl">My Comment</Link>
+            </div>
         </div>
     )
 }
